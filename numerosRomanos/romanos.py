4aa32f44fa20dd37fd3ca430bbcc4ed01c0fe8e4
Jonnathan Ng <ngjonnathan@gmail.com>
2015-02-08 20:55:49 -0430

    Incremento 4

--- numerosRomanos/romanos.py
+++ numerosRomanos/romanos.py
@@ -4,7 +4,12 @@ Created on 8/2/2015
 @author: Jonnathan
 '''
 def convertirRomano(numero):
-    pass
+    if numero == 'I':
+        return 1
+    elif numero == 'V':
+        return 5
+    else:
+        return 10
 
 if __name__ == '__main__':
     pass
--- numerosRomanos/testNumeroRomanos.py
+++ numerosRomanos/testNumeroRomanos.py
@@ -10,6 +10,18 @@ class Test(unittest.TestCase):
 
     def testRomanoExiste(self):
         numero = convertirRomano('I')
+    
+    def testNumeroUno(self):
+        numero = convertirRomano('I')
+        self.assertEqual(numero, 1)
+    
+    def testNumeroCinco(self):
+        numero = convertirRomano('V')
+        self.assertEqual(numero, 5)
+           
+    def testNumeroDiez(self):
+        numero = convertirRomano('X')
+        self.assertEqual(numero, 10)
         
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
